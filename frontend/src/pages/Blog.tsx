ad50f4cad2716c9832d887ddb716cba0d437f1e5
395ShikharSingh <shikharpersonal8@gmail.com>
2025-01-29 23:58:35 +0530

    Type Error

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -5,29 +5,23 @@ import { Spinner } from "../components/Spiner";
 import { Appbar } from "../components/Appbar";
 
 export const Blog = () => {
-    const { id } = useParams();
-    const { loading, blog } = useBlog({ id: id || "" });
-
-    if (loading) {
-        return (
-            <div>
-                <Appbar />
-                <div className="h-screen flex flex-col justify-center">
-                    <div className="flex justify-center">
-                        <Spinner />
-                    </div>
+    const { id } = useParams()
+    const {loading, blog}= useBlog({
+        id: id || ""
+    });
+    if(loading || !blog) {
+        return <div>
+            <Appbar />
+            <div className="h-screen flex flex-col justify-center">
+                <div className="flex justify-center">
+                    <Spinner/>
                 </div>
             </div>
-        );
-    }
-
-    if (!blog) {
-        return <div>Error: Blog not found</div>;  // Handling case when blog is undefined
-    }
-
-    return (
-        <div>
-            <FullBlog blog={blog} />
         </div>
-    );
-};
+        
+    }
+
+    return <div>
+       <FullBlog blog={blog}/>
+    </div>
+}
